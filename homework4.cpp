8178f282a4ad37020b2d22f11e00304bfc77ef92
seongjuNam <nsj7727@naver.com>
2022-10-12 05:02:28 +0900

    Update homework4.cpp

--- homework4.cpp
+++ homework4.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
-    long double flo;
+    long double floatt;
 
-    int flo1;       //부동소수의 정수부분
+    int floatt1;       //부동소수의 정수부분
     long double flo2;     //부동소수의 소수부분
 
     cout << "부동 소수점을 입력하세요: ";
     cin >> flo;
     
-    flo1 = int(flo);     //정수부분
-    flo2 = flo - flo1;   //소수부분
+    flo1 = int(floatt);     //정수부분
+    flo2 = floatt - flo1;   //소수부분
 
-    cout << "처음입력하신 부동소수는: " << flo << "입니다."<< endl;
-    cout << "정수부분은: " << flo1 << "입니다." << endl;
+    cout << "처음입력하신 부동소수는: " << floatt << "입니다."<< endl;
+    cout << "정수부분은: " << att1 << "입니다." << endl;
     cout << "소수부분은: " << flo2 << "입니다." ;
-
+    cout << "안녕하세요 ㅎㅎ";
     return 0;
 }
